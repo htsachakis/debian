39ad598b0e8f6a627f474f8f1a68a8d48bd52abb
Charalampos Tsachakis <htsachakis@gmail.com>
2024-06-17 13:49:32 +0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -3,6 +3,6 @@
 # EXA installation
 # replace ls command in .bashrc file with line below
 # alias ls='exa -al --long --header --color=always --group-directories-first' 
-sudo apt install -y exa
+sudo apt install -y exa curl tree git
 
 cp .bash_aliases ~/.bash_aliases
