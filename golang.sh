6649ec056996d0c7182bb3b5e44cb034d0b2d94b
Charalampos Tsachakis <htsachakis@gmail.com>
2024-06-17 13:38:17 +0300

    Update golang.sh

--- golang.sh
+++ golang.sh
@@ -2,5 +2,6 @@
 cd /tmp
 wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.4.linux-amd64.tar.gz
+# Edit ~/.profile and add in the end
 # export PATH=$PATH:/usr/local/go/bin
 go version
