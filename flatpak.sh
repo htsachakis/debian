46f5aafccabf5d6999683c9f06739117aea32df1
Charalampos Tsachakis <htsachakis@gmail.com>
2024-06-19 13:37:31 +0300

    flatpack update

--- flatpak.sh
+++ flatpak.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo apt install flatpak -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 flatpak install -y com.github.hluk.copyq
 flatpak install -y com.github.tchx84.Flatseal
 flatpak install -y com.google.Chrome
