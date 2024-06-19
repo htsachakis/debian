75ad387f59c44f3fb171b3f0049908c91b92ce44
Charalampos Tsachakis <htsachakis@gmail.com>
2024-06-19 11:09:13 +0300

    Create flatpak.sh

---
+++ flatpak.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+flatpak install -y com.github.hluk.copyq
+flatpak install -y com.github.tchx84.Flatseal
+flatpak install -y com.google.Chrome
+flatpak install -y com.microsoft.Edge
+flatpak install -y com.visualstudio.code
